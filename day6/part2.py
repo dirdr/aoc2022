ac4f1e7938b99c9e08784b578fe344b293470b53
= <adrien.pelfresne@gmail.com>
2022-12-06 21:04:17 +0100

    day 6

---
+++ day6/part2.py
@@ -0,0 +1,25 @@
+def solve() -> int:
+    with open('input.txt', 'r') as file:
+        line = file.readlines()[0].rstrip()
+        print(line[1230:])
+        cons: int = 0
+        seen = set()
+        count: int = 0
+        for char in line:
+            if char in seen:
+                seen.clear()
+                seen.add(char)
+                cons = 1
+            else:
+                cons += 1
+                if cons == 14:
+                    return count;
+                seen.add(char)
+            count += 1
+    return -1
+
+def main() -> None:
+    print(solve())
+
+if __name__ == '__main__':
+    main()
